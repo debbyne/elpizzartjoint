2dfcd8d01b44448661fd684cdf31baf8f8607956
debbyne <kinyadebbyne@gmail.com>
2022-01-16 16:10:18 +0300

    add opacity

---
+++ script.js
@@ -0,0 +1,11 @@
+function orderPizza(size,crust,toppings,quantity,totalPrice){
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.quantity = quantity;
+    this.totalPrice = totalPrice;
+}
+
+$(document).ready(function (){
+$
+})
