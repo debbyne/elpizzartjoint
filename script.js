320761d774f72f26a3c1198bb3f95df3ef9bd5aa
debbyne <kinyadebbyne@gmail.com>
2022-01-16 23:02:42 +0300

    add forms

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function orderPizza(size,crust,toppings,quantity,totalPrice){
     this.totalPrice = totalPrice;
 }
 
-$(document).ready(function (){
-$
+var pizzaSize
+var pizzaCrust
+var pizzaToppings
+var pizzaQuantity
+var totalPrice
+
+$(document).ready(function(){
+   $ ("#placeOrder").click(function(){
+    pizzaSize= parseInt($("form input[name='size']:checked").val());
+    pizzaCrust = parseInt($("form input[name='crust']:checked").val());
+    pizzaToppings = parseInt($("form input[name='toppings']:checked").val());
+    pizzaQuantity= parseInt($("form input[name='quantity']").val());
+   })
 })
